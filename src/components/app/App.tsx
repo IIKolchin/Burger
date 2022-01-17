cab8c98615e0b63dac6c35d679c7a2d9eb6f950a
Apple <apple@192.168.1.4>
2022-01-18 01:00:05 +0300

    get data from api

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,11 +1,32 @@
 import React from "react";
-import { data } from "../../utils/data";
+import { url } from "../../utils/data";
 import styles from "./app.module.css";
 import AppHeader from "../app-header/app-header";
 import BurgerIngredients from "../burger-ingredients/burger-ingredients";
 import BurgerConstructor from "../burger-constructor/burger-constructor";
 
 function App() {
+  const [state, setState] = React.useState({
+    hasError: false,
+    data: [],
+  });
+
+  const { data, hasError } = state;
+
+  React.useEffect(() => {
+    getIngredients();
+  }, []);
+
+  function getIngredients() {
+    setState({ ...state, hasError: false });
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setState({ ...state, data: data.data }))
+      .catch((e) => {
+        setState({ ...state, hasError: true });
+      });
+  }
+
   return (
     <>
       <AppHeader />
--- src/components/burger-ingredients/burger-ingredients.js
+++ src/components/burger-ingredients/burger-ingredients.js
@@ -9,6 +9,9 @@ import PropTypes from "prop-types";
 import { dataPropTypes } from "../../utils/data";
 
 function BurgerIngredients(props) {
+  const buns = props.data.filter((item) => item.type === "bun")
+  const sauces = props.data.filter((item) => item.type === "sauce")
+  const mains = props.data.filter((item) => item.type === "main")
   const [current, setCurrent] = React.useState("one");
   return (
     <section>
@@ -27,9 +30,7 @@ function BurgerIngredients(props) {
       <div className={styles.ingredients + " mt-10"}>
         <h2 className={styles.subtitle + " mb-6"}>Булки</h2>
         <ul className={styles.ul + " ml-4"}>
-          {props.data
-            .filter((item) => item.type.includes("bun"))
-            .map((data) => {
+          {buns.map((data) => {
               return (
                 <li key={data._id} className={styles.li}>
                   {data._id === "60666c42cc7b410027a1a9b1" ? (
@@ -50,9 +51,7 @@ function BurgerIngredients(props) {
 
         <h2 className={styles.subtitle + " mt-10 mb-6"}>Соусы</h2>
         <ul className={styles.ul + " ml-4"}>
-          {props.data
-            .filter((item) => item.type.includes("sauce"))
-            .map((data) => {
+          {sauces.map((data) => {
               return (
                 <li key={data._id} className={styles.li}>
                   {data._id === "60666c42cc7b410027a1a9b9" ? (
@@ -73,9 +72,7 @@ function BurgerIngredients(props) {
 
         <h2 className={styles.subtitle + " mt-10 mb-6"}>Начинки</h2>
         <ul className={styles.ul + " ml-4"}>
-          {props.data
-            .filter((item) => item.type.includes("main"))
-            .map((data) => {
+          {mains.map((data) => {
               return (
                 <li key={data._id} className={styles.li}>
                   <img className="ml-4 mr-4" src={data.image} alt={data.name} />
