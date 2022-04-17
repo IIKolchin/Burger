1976c1442fde7b69c51bc2ac87efcea7ba5fac77
Apple <IIKolchin@yandex.ru>
2022-04-17 19:46:59 +0300

    fix app-header

--- src/components/app-header/app-header.jsx
+++ src/components/app-header/app-header.jsx
@@ -9,57 +9,71 @@ import {
 import { NavLink } from "react-router-dom";
 
 function AppHeader() {
-  
   return (
     <header className={styles.header}>
       <nav className={styles.nav}>
         <ul className={styles.menu}>
-          <li className={styles.constructor}>
-            <BurgerIcon
-              type={window.location.pathname === "/" ? "primary" : "secondary"}
-            />
-            <NavLink
-              to="/"
-              className={
-                styles.link + " text text_type_main-default ml-2 mr-10 mr-2"
-              }
-              activeClassName={styles.activeLink}
-              exact
-            >
-              Конструктор
-            </NavLink>
 
-            <a href="#" className={styles.link}>
+          <NavLink
+            to="/"
+            className={styles.link + " text text_type_main-default mr-10 "}
+            activeClassName={styles.activeLink}
+            exact
+          >
+            <li className={styles.constructor}>
+              <BurgerIcon
+                type={
+                  window.location.pathname === "/" ? "primary" : "secondary"
+                }
+              />
+              <div className="ml-2">Конструктор</div>
+            </li>
+
+          </NavLink>
+
+          <NavLink
+            to="/feed"
+            className={
+              styles.link + " text text_type_main-default text_color_inactive ml-2"
+            }
+            activeClassName={styles.activeLink}
+            exact
+          >
+            <li className={styles.constructor}>
               <div className={styles.burger}>
-                <ListIcon type={window.location.pathname === "/feed" ? "primary" : "secondary"} />
+                <ListIcon
+                  type={
+                    window.location.pathname === "/feed"
+                      ? "primary"
+                      : "secondary"
+                  }
+                />
               </div>
-              <NavLink to="/feed" className={styles.link + " text text_type_main-default text_color_inactive ml-2"} activeClassName={styles.activeLink}
-              exact>
-                Лента заказов
-              </NavLink>
-            </a>
-          </li>
+              <div className="ml-2">Лента заказов</div>
+            </li>
+          </NavLink>
+
           <li className={styles.logo}>
             <Logo />
           </li>
 
-          <li className={styles.profile}>
-            <ProfileIcon
-              type={
-                window.location.pathname === "/profile" ||
-                window.location.pathname === "/profile/orders"
-                  ? "primary"
-                  : "secondary"
-              }
-            />
-            <NavLink
-              to={{ pathname: "/profile" }}
-              className={styles.link + " text text_type_main-default ml-2"}
-              activeClassName={styles.activeLink}
-            >
-              Личный кабинет
-            </NavLink>
-          </li>
+          <NavLink
+            to={{ pathname: "/profile" }}
+            className={styles.link + " text text_type_main-default"}
+            activeClassName={styles.activeLink}
+          >
+            <li className={styles.profile}>
+              <ProfileIcon
+                type={
+                  window.location.pathname === "/profile" ||
+                  window.location.pathname === "/profile/orders"
+                    ? "primary"
+                    : "secondary"
+                }
+              />
+              <div className="ml-2">Личный кабинет</div>
+            </li>
+          </NavLink>
         </ul>
       </nav>
     </header>
